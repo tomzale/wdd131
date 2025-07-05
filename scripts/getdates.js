a6812adfa6f280d4c591f01c1c6b726ae9b595a9
Thomas Etiuzale <faithfulmissioner@gmail.com>
2025-07-05 23:31:52 +0100

    initial commit for my wdd131 course

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// For The current year
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+// For the last modified date
+document.querySelector("#lastModified").textContent = `Last Modified: ${document.lastModified}`;
